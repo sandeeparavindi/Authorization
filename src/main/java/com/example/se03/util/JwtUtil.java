7a0b805bb82c0b909c283e2762648fdae1bec493
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-08-21 11:20:34 +0530

    UserServiceImpl

--- src/main/java/com/example/se03/util/JwtUtil.java
+++ src/main/java/com/example/se03/util/JwtUtil.java
@@ -1,2 +1,18 @@
-package com.example.se03.util;public class JwtUtil {
+package com.example.se03.util;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+import java.io.Serializable;
+
+@Component
+@PropertySource(ignoreResourceNotFound = true,
+value = "classpath:otherprops.properties")
+
+public class JwtUtil implements Serializable {
+    private static final long serialVersionUTD = 234234523523L;
+    public static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60 * 12;
+    @Value("${jwt.secret}")
+    private String secretKey;
 }
