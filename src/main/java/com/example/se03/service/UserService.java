ad06640ef347764622fb5b7c5bd0cc435b9eae14
sandeeparavindi <sandeeparavindi1022@gmail.com>
2024-08-21 10:17:53 +0530

    Model Mapper

--- src/main/java/com/example/se03/service/UserService.java
+++ src/main/java/com/example/se03/service/UserService.java
@@ -1,2 +1,8 @@
-package com.example.se03.service;public interface UserService {
+package com.example.se03.service;
+
+import com.example.se03.dto.UserDTO;
+
+public interface UserService {
+    int saveUser(UserDTO userDTO);
+    UserDTO serachUser(String username);
 }
